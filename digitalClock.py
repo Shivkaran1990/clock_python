1dfda6760ded7ca63c6d61511500108f458998fb
Shivkaran1990 <s.ravidas01@gmail.com>
2024-06-27 21:39:41 +0530

    add final clock in clock branch

--- digitalClock.py
+++ digitalClock.py
@@ -2,7 +2,11 @@ import tkinter as tk
 from tkinter import Label
 import time
 
-
+# Function to update the time
+def time_update():
+    current_time = time.strftime("%H:%M:%S %p")
+    clock_label.config(text=current_time)
+    clock_label.after(1000, time_update)  # Update the time every second
 
 # Create the main window
 root = tk.Tk()
@@ -12,7 +16,8 @@ root.title("Digital Clock")
 clock_label = Label(root, font=("Helvetica", 48), background="black", foreground="white")
 clock_label.pack(anchor='center')
 
-
+# Start the clock
+time_update()
 
 # Run the Tkinter event loop
 root.mainloop()
